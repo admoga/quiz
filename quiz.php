ed8f2e4187dc38fa8d9edb643ec77414bdaf5dc9
admoga <admoga01@gmail.com>
2018-03-28 15:46:24 +0200

    finalizado

--- quiz.php
+++ quiz.php
@@ -15,8 +15,6 @@
     <link href="../assets/css/style.css" rel="stylesheet" type="text/css"/>
     <script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
     <script type="text/javascript" src="assets/js/controller.js" data-lang="<?php echo $lang; ?>"></script>
-    <script type="text/javascript" src="assets/js/contact.js"></script>
-    
 
 
 </head>
